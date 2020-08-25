a9bef99689bdf9d26f5dbf61adb9c94838e00226
Cullen Andrews <66756039+cullen-andrews@users.noreply.github.com>
2020-08-25 14:20:03 -0700

    outputs and queries up to number 3

--- EmployeeSQL/sql/queries.sql
+++ EmployeeSQL/sql/queries.sql
@@ -12,3 +12,23 @@ SELECT * FROM salaries;
 
 SELECT * FROM titles;
 
+--List the following details of each employee: employee number, last name, first name, sex, and salary.
+
+SELECT employees.emp_no, employees.last_name, employees.first_name, employees.sex, salaries.salary 
+FROM employees INNER JOIN salaries ON employees.emp_no=salaries.emp_no;
+
+--List first name, last name, and hire date for employees who were hired in 1986.
+
+SELECT first_name, last_name, hire_date FROM employees
+WHERE hire_date BETWEEN '1986-01-01' AND '1986-12-31';
+
+--List the manager of each department with the following information: department number, 
+--department name, the manager's employee number, last name, first name.
+
+SELECT dept_manager.dept_managed, departments.dept_name, dept_manager.man_id, employees.last_name, employees.first_name
+FROM 
+dept_manager JOIN departments 
+ON dept_manager.dept_managed=departments.dept_no
+JOIN employees
+ON dept_manager.man_id=employees.emp_no;
+
