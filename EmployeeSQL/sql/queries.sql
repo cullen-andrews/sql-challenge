38ef091bbcaebc370096eb8c7886f23dd212d52e
Cullen Andrews <66756039+cullen-andrews@users.noreply.github.com>
2020-08-25 14:32:03 -0700

    progress up to number 4

--- EmployeeSQL/sql/queries.sql
+++ EmployeeSQL/sql/queries.sql
@@ -32,3 +32,10 @@ ON dept_manager.dept_managed=departments.dept_no
 JOIN employees
 ON dept_manager.man_id=employees.emp_no;
 
+--List the department of each employee with the following information: employee number, last name, first name, and department name.
+SELECT dept_emp.emp_no, employees.last_name, employees.first_name, departments.dept_name
+FROM
+dept_emp JOIN employees
+ON dept_emp.emp_no=employees.emp_no
+JOIN departments
+ON dept_emp.dept_no=departments.dept_no;
